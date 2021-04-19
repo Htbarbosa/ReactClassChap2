b022afabd7015c96f877fecc98b4ee7e9f794d83
Henrique Teles Barbosa <henrique_telesbarbosa@hotmail.com>
2021-04-19 17:16:06 -0300

    fake api setup

--- src/componensts/TransactionsTable/index.tsx
+++ src/componensts/TransactionsTable/index.tsx
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
-export function TransactionsTable(){
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td  className="withdraw">- R$ 1.100</td>
-                        <td>Moradia</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    );
+export function TransactionsTable() {
+    useEffect(() => {
+        api.get('transactions')
+            .then(response => console.log(response.data));
+}, []);
+return (
+    <Container>
+        <table>
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Valor</th>
+                    <th>Categoria</th>
+                    <th>Data</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Desenvolvimento de website</td>
+                    <td className="deposit">R$ 12.000</td>
+                    <td>Desenvolvimento</td>
+                    <td>20/02/2021</td>
+                </tr>
+                <tr>
+                    <td>Aluguel</td>
+                    <td className="withdraw">- R$ 1.100</td>
+                    <td>Moradia</td>
+                    <td>20/02/2021</td>
+                </tr>
+            </tbody>
+        </table>
+    </Container>
+);
 }
